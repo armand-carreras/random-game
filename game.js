22733b9dbed10f422815da4aba34e2090a778aff
Armand <armand.carreras@student.bit.es>
2020-10-06 13:35:49 +0200

    Styled

--- game.js
+++ game.js
@@ -19,14 +19,6 @@ function array_add(num){
     }
 }
 
-var input = document.getElementById("number");
-input.addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        document.getElementById("add_btn").click();}})
-
-
-
 function restart(){
     document.getElementById("restart_btn").hidden = true;
     x=0;
@@ -39,9 +31,11 @@ function restart(){
         }
     document.getElementById("send_btn").disabled = true;
     document.getElementById("Result").innerHTML = "";
+    document.getElementById("send_btn").hidden = false;
 }
 function reveal(){
     document.getElementById("restart_btn").hidden = false;
+    document.getElementById("send_btn").hidden = true;
     if(toggle){document.getElementById("Result").innerHTML = "Congrats the number was :" + rand;}
     else{document.getElementById("Result").innerHTML = "The number was " + rand + " try again."}
     
